212a4179cd56dc9a04b76bba542eed9e103c21f5
Igor soares <igoranalista01@gmail.com>
2026-06-15 08:33:08 -0300

    Atividade de condicionais, Monitorando vendas no comercio

---
+++ condionais,if,elif,else/monitorando vendas.py
@@ -0,0 +1,9 @@
+macas = int(input('Digite a quanitade de maçãs vendidas: '))
+bananas = int(input('Digite a quanitade de bananas vendidas: '))
+
+if macas > bananas:
+    print('Maças tiveram mais vendas')
+elif macas < bananas:
+    print('Bananas tiveram mais vendas')
+else:
+    print('As vendas foram iguais')        
